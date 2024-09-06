6983689d83129fc2e35970e403f94e74ef8b44ed
Minmitha <minmitha@cloudrevelinnovation.com>
2024-09-06 17:28:11 +0530

    button changes

---
+++ src/Components/LongButton.js
@@ -0,0 +1,33 @@
+import '../App.css';
+export function LongButton() {
+    return (
+        <>
+            <div>
+                <button class="Btn-Container flex w-[170px] h-auto bg-[#0a0a0a] rounded-full shadow-md justify-between items-center  cursor-pointer">
+                    <span class="text text-white text-xl tracking-wide flex-1 h-full flex items-center justify-center">let's go!</span>
+                    <span class="icon-Container w-[45px] h-[45px] bg-[#FFB400] flex items-center justify-center rounded-full border-[3px] border-[#1d2129]">
+                        <svg
+                            width="16"
+                            height="19"
+                            viewBox="0 0 16 19"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <circle cx="1.61321" cy="1.61321" r="1.5" fill="white"></circle>
+                            <circle cx="5.73583" cy="1.61321" r="1.5" fill="white"></circle>
+                            <circle cx="5.73583" cy="5.5566" r="1.5" fill="white"></circle>
+                            <circle cx="9.85851" cy="5.5566" r="1.5" fill="white"></circle>
+                            <circle cx="9.85851" cy="9.5" r="1.5" fill="white"></circle>
+                            <circle cx="13.9811" cy="9.5" r="1.5" fill="white"></circle>
+                            <circle cx="5.73583" cy="13.4434" r="1.5" fill="white"></circle>
+                            <circle cx="9.85851" cy="13.4434" r="1.5" fill="white"></circle>
+                            <circle cx="1.61321" cy="17.3868" r="1.5" fill="white"></circle>
+                            <circle cx="5.73583" cy="17.3868" r="1.5" fill="white"></circle>
+                        </svg>
+                    </span>
+                </button>
+
+            </div>
+        </>
+    )
+}
