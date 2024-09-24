ef6b2eb8d7e34b6342440a05e83e76976081808d
Minmitha <minmitha@cloudrevelinnovation.com>
2024-09-24 13:14:48 +0530

    vercel updates

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { Bio } from './Components/Bio';
 import { About } from './Components/About';
 import { Portfolio } from "./Components/Portfolio";
 import { ContactMe } from "./Components/ContactMe";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
+
 
 const App = () => {
   return (
     <>
+      <SpeedInsights />
+      <Analytics/>
       <Routes>
         <Route path="/" element={<Bio />} />
         <Route path="/about" element={<About />} />
