34ef5e6e74c5eb66b5e81cc854d053e68888345c
Minmitha <minmitha@cloudrevelinnovation.com>
2024-09-25 17:45:34 +0530

    transition colors changed

--- src/Components/TransitionMotion.js
+++ src/Components/TransitionMotion.js
@@ -4,22 +4,22 @@ const TransitionMotion = (OgComponent) => {
     <div>
       <OgComponent />
       <motion.div
-        className="fixed top-0 left-0 w-full h-full bg-black origin-bottom"
+        className="fixed top-0 left-0 w-full h-screen bg-yellow-500 origin-bottom"
         initial={{ scaleY: 0 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 1 }}
         transition={{
-          duration: 1,
+          duration: 0.5,
           ease: [0.22, 1, 0.36, 1]
         }}
       />
       <motion.div
-        className="fixed top-0 left-0 w-full h-full bg-black origin-top"
+        className="fixed top-0 left-0 w-full h-screen bg-black origin-top"
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
         transition={{
-          duration: 1,
+          duration: 0.5,
           ease: [0.22, 1, 0.36, 1]
         }}
       />
